c9f8786ecc44637c6494bae7e35c03b8bde1c920
Unknown <prpr48@naver.com>
2020-02-29 13:26:48 +0900

    button css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './App.css';
 
 function App() {
-  return <div className="App">Learn React</div>;
+  return <button className="button">Hello</button>;
 }
 
 export default App;
