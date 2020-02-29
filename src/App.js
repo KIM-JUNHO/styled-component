cfd0d28b81d2eada06e9c2395e0f85f7bdb1a203
Unknown <prpr48@naver.com>
2020-02-29 13:57:36 +0900

    remove button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, { createGlobalStyle, css, keyframes } from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
 body {
@@ -12,13 +12,7 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <Container>
-        <Button>Hello</Button>
-        <Button danger rotationTime={5}>
-          Hello
-        </Button>
-        <Anchor gref="http://google.com">Go to google</Anchor>
-      </Container>
+      <Container></Container>
     </>
   );
 }
@@ -27,41 +21,6 @@ const Container = styled.div`
   height: 100vh;
   width: 100%;
   background-color: pink;
-`;
-
-const Button = styled.button`
-  border-radius: 50px;
-  padding: 5px;
-  min-width: 120px;
-  color: white;
-  font-weight: 600;
-  -webkit-appearance: none;
-  cursor: pointer;
-  &:active,
-  &:focus {
-    outline: none;
-  }
-  background-color: ${props => (props.danger ? '#e74c3c' : '#2ecc71')};
-  ${props => {
-    if (props.danger) {
-      return css`
-        animation: ${rotation} ${props.rotationTime}s linear infinite;
-      `;
-    }
-  }}
-`;
-const Anchor = styled(Button.withComponent('a'))`
-  text-decoration: none;
-`;
-
-const rotation = keyframes`
-from {
-  transform: rotate(0deg);
-}
-to {
-  transform: rotate(360deg);
-  
-}
 `;
 
 export default App;
