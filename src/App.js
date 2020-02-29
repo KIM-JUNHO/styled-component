55d05d465f5d4af786ccb3516df35979b3b684a0
Unknown <prpr48@naver.com>
2020-02-29 14:03:01 +0900

    sass mixin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle, css } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
 body {
@@ -8,10 +8,25 @@ body {
 }
 `;
 
+const awesomeCard = css`
+  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px;
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  ${awesomeCard}
+  background-color: pink;
+`;
+
 const Input = styled.input.attrs({
   required: true
 })`
-  border-radius: 5px;
+  border: none;
+  ${awesomeCard}
 `;
 
 function App() {
@@ -24,11 +39,5 @@ function App() {
     </>
   );
 }
-
-const Container = styled.div`
-  height: 100vh;
-  width: 100%;
-  background-color: pink;
-`;
 
 export default App;
